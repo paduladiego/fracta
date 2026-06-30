e12924233bea6f7a3f59d8287a91157d0c445027
paduladiego <padualdiego@gmail.com>
2026-06-30 19:21:57 -0300

    chore: inicializa branch v4 e atualiza versao do app para 2.0.0

--- core/constants.py
+++ core/constants.py
@@ -21,7 +21,7 @@ SAVE_PARAMETERS = {
 }
 
 # Configurações de Versionamento e Atualização Remota (Multi-plataforma)
-APP_VERSION = "1.0.0"
+APP_VERSION = "2.0.0"
 UPDATE_JSON_URL = "https://apps.dula.one/fracta/version.json"
 
 
